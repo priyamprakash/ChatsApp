98d4e69e2b32dd7eaacbae8b8c8cf07f47e49a01
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-04 05:32:44 +0530

    phonenumber xml

---
+++ app/src/main/java/com/app/chatsapp/PhoneNumberActivity.java
@@ -0,0 +1,15 @@
+
+package com.app.chatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+    }
+}
