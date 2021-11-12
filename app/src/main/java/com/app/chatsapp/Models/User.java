9572b05d51400cc87456e2ac2491b2fa10e509e9
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-12 23:24:01 +0530

    Update User.java

--- app/src/main/java/com/app/chatsapp/Models/User.java
+++ app/src/main/java/com/app/chatsapp/Models/User.java
@@ -14,4 +14,43 @@ public class User {
         this.profileImage = profileImage;
     }
 
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
